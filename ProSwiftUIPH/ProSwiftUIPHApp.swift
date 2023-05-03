6cb2e503ebe371df30cb4592653a4775a38172e9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-03 13:43:41 -0600

    Initial Commit

---
+++ ProSwiftUIPH/ProSwiftUIPHApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProSwiftUIPHApp.swift
+//  ProSwiftUIPH
+//
+//  Created by Mitch Andrade on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ProSwiftUIPHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
