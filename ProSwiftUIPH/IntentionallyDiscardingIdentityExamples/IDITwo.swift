194896c7f7cebf9ea5501c85037c52d60570a915
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-04 17:23:52 -0600

    Finished 1-7: Intentionally discarding identity of Pro SwiftUI

--- ProSwiftUIPH/IntentionallyDiscardingIdentityExamples/IDITwo.swift
+++ ProSwiftUIPH/IntentionallyDiscardingIdentityExamples/IDITwo.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct IDITwo: View {
     let colors: [Color] = [.blue, .cyan, .gray, .indigo, .mint, .orange, .pink, .purple, .red]
+    let symbols = ["run", "archery", "basketball", "bowling", "dance", "golf", "hiking", "jumprope", "rugby", "tennis", "volleyball", "yoga"]
+    @State private var id = UUID()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(colors.randomElement()!)
+                    .padding()
+                
+                Image(systemName: "figure.\(symbols.randomElement()!)")
+                    .font(.system(size: 144))
+                    .foregroundColor(.white)
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Button("Change") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    id = UUID()
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.bottom)
+        }
     }
 }
 
