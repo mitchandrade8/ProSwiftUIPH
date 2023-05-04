945e75a8759f944b9feed020cf96e8c2d7d7f858
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-04 16:03:58 -0600

    Learning about intentionally discarding identity and created a new folder with examples

---
+++ ProSwiftUIPH/IntentionallyDiscardingIdentityExamples/IDITwo.swift
@@ -0,0 +1,21 @@
+//
+//  IDITwo.swift
+//  ProSwiftUIPH
+//
+//  Created by Mitch Andrade on 5/4/23.
+//
+
+import SwiftUI
+
+struct IDITwo: View {
+    let colors: [Color] = [.blue, .cyan, .gray, .indigo, .mint, .orange, .pink, .purple, .red]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IDITwo_Previews: PreviewProvider {
+    static var previews: some View {
+        IDITwo()
+    }
+}
